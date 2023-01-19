e6e04edfe0e9f1e0eb7c256f1d6212f35dddb2aa
Kutay Tıstık <kutaaytistik99@outlook.com>
2023-01-19 12:34:18 +0300

    19.01.2022

--- DataAccess/Concrete/EntityFramework/RentACarContext.cs
+++ DataAccess/Concrete/EntityFramework/RentACarContext.cs
@@ -10,7 +10,7 @@ namespace DataAccess.Concrete.EntityFramework
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=DbReCapProject2;Trusted_Connection=true");
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=RentACar;Trusted_Connection=true");
         }
 
         public DbSet<Car> Cars { get; set; }
